a13390df963772fa50ff3380e5915734a43e2825
unknown <teslagosu@gmail.com>
2020-09-01 18:01:07 +0200

    Added comments in Airport Class

--- src/Model/Airport.java
+++ src/Model/Airport.java
@@ -5,21 +5,23 @@ import Controller.AirportController;
 import java.util.HashMap;
 
 public class Airport {
+    //variables
     private int gate;
     private String airportName;
     private Airline airline;
 
 
-
+    //constructor when instanciating an Airport
     public Airport(){
         AirportController airportController = new AirportController();
         airline = new Airline();
-        for (Airport a: airportController.getAirportList()) {
+        for (Airport a: airportController.getAirportList()) { //print out available airports
             System.out.println(a.airportName);
         }
 
     }
 
+    //getters and setters
     public Airport(String airportName){
         this.airportName = airportName;
     }
@@ -27,8 +29,6 @@ public class Airport {
     public String getAirportName() {
         return airportName;
     }
-
-
 
     public void setGate(int gate) {
         this.gate = gate;
