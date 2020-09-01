7e3fb5dfd2a7637e8177a9f7ec53fd5a66e3314b
unknown <teslagosu@gmail.com>
2020-09-01 17:57:20 +0200

    Added comments in Country Class

--- src/Model/Country.java
+++ src/Model/Country.java
@@ -1,13 +1,16 @@
 package Model;
 
 public class Country {
+    //variables
     private String city;
-    private String location;
+    private String location; //Country
 
+    //constructor when creating a instance of a country.
     public Country(String city, String location){
         this.city = city;
     }
 
+    //getters and setters for variables
     public String getCity() {
         return city;
     }
@@ -15,7 +18,6 @@ public class Country {
     public void setCity(String city) {
         this.city = city;
     }
-
 
     public String getLocation() {
         return location;
