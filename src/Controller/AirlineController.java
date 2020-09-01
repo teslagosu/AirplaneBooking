920ddd92899a19f687bda05f99e1668b4f86682f
unknown <teslagosu@gmail.com>
2020-09-01 18:06:10 +0200

    Added comments in AirlineController Class

--- src/Controller/AirlineController.java
+++ src/Controller/AirlineController.java
@@ -7,14 +7,16 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AirlineController {
-
+    //variables
     Message msg = new Message();
+    //arraylists
     private ArrayList<Airline> airlineList = new ArrayList<>();
 
+    //Constructor
     public AirlineController(){
         this.createAirline();
     }
-
+    //Creates an airline
     public void createAirline(){
         msg.AirlineNameOfAirLineQuestion();
         String airlineName = getAirLineName();
@@ -22,12 +24,12 @@ public class AirlineController {
         airline.setAirlineName(airlineName);
         airlineList.add(airline);
     }
-
+    //takes an input from user for specified airline name
     public String getAirLineName(){
         Scanner input = new Scanner(System.in);
         return input.nextLine();
     }
-
+    //returns list of airlines.
     public ArrayList<Airline> getAirlineList() {
         return airlineList;
     }
